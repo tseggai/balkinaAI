4e0d31b3cf3481a8128d98bad53cce606da44580
Tseggai Debrezion <tseggai@gmail.com>
2026-03-01 00:41:41 +0100

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   transpilePackages: ['@balkina/shared', '@balkina/db', '@balkina/config'],
 };
 
 export default nextConfig;
+
