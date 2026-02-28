ffebd758236f67620b6e59914fd227b653633d15
Tseggai Debrezion <tseggai@gmail.com>
2026-03-01 00:45:08 +0100

    Update and rename next.config.ts to next.config.mjs

---
+++ apps/admin/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@balkina/shared', '@balkina/db', '@balkina/config'],
+};
+
+export default nextConfig
--- apps/admin/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  transpilePackages: ['@balkina/shared', '@balkina/db', '@balkina/config'],
-};
-
-export default nextConfig;
